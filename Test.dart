b876f07bc75d97ee077c2c1c361de2ef5c3896e4
Andres <aodiard@yahoo.com>
2022-05-10 18:34:08 -0300

    Ejercicios Dart

---
+++ Test.dart
@@ -0,0 +1,11 @@
+// importing dart:io file
+import 'dart:io';
+
+void main() {
+  print("Enter your name?");
+  // Reading name of the Geek
+  String? name = stdin.readLineSync();
+
+  // Printing the name
+  print("Hello, $name! \nWelcome to GeeksforGeeks!!");
+}
